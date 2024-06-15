f2dbfc381fefdb673f6d9b4091e80440393a94ba
amit kumar <amitkumar.cs19@bmsce.ac.in>
2024-06-15 21:44:56 +0530

    firebase model , add/get blogs functionality

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
 
 
@@ -18,5 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 // const analytics = getAnalytics(app);
---
+++ src/firebase/interface.tsx
@@ -0,0 +1,11 @@
+export interface Blog {
+    author: string;
+    image: string;
+    content: string;
+    summary: string;
+    keywords: string[];
+    doe: Date;
+    dop: Date;
+    premium: boolean;
+    coins: number;
+  }
