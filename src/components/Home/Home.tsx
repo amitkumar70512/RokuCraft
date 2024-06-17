e6eb12eb371ae283933ccf0603dee844d2d2ba61
Shivp436 <mpshivanandpatil2002@gmail.com>
2024-06-18 00:34:47 +0530

    Added Continue Reading

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,14 @@
-import RecentRead from "../Blogs/RecentRead";
+import ContinueReading from "../Blogs/ContinueReading";
 import LatestBlogs from "../Blogs/LatestBlogs";
 import React from "react";
 import FeaturedBlogs from "../Blogs/FeaturedBlogs";
 import Sidebar from "../Sidebar/Sidebar";
 
+
 function Home() {
 	return (
 		<>
-			<RecentRead />
+			<ContinueReading/>
 			<LatestBlogs />
 			<h2>Featured Blogs</h2>
 			<FeaturedBlogs />
