782758439cee10736acfd5f26d78cb8995b836ae
amit kumar <amitkumar.cs19@bmsce.ac.in>
2024-06-20 23:11:02 +0530

    added loader while loading

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -5,15 +5,17 @@ import { getAllBlogs } from '../../firebase/blogs';
 
 export default function Blogs() {
     const [blogData, setBlogData] = useState<Blog[]>([]); // State to hold blog data
+    const [loading, setLoading] = useState<boolean>(true); // State to manage loading state
 
     useEffect(() => {
         const fetchBlogs = async () => {
             try {
                 const data = await getAllBlogs();
                 setBlogData(data); // Update state with fetched data
-                console.log(data);
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching blogs:', error);
+                setLoading(false);
                 // Handle error (e.g., show error message)
             }
         };
@@ -28,9 +30,22 @@ export default function Blogs() {
                 <div className="container">
                     {/* Render BlogCard components dynamically */}
                     <div className="row">
-                        {blogData.map((blog) => (
+                        {!loading && blogData && blogData.map((blog) => (
                             <BlogCard blog={blog} key={blog.id} />
                         ))}
+                        {
+                            loading && !blogData &&
+                            <div className="spinner-border text-secondary" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+
+                        }
+                        {
+                            !blogData && !loading &&
+                            <div className="alert alert-danger" role="alert">
+                                Blog Content Not Found
+                            </div>
+                        }
                     </div>
                     <nav aria-label="Page navigation example">
                         <ul className="pagination justify-content-center">
