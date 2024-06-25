4a61832e195b050c0738872d4db8b63bb10e819d
amit kumar <amitkumar.cs19@bmsce.ac.in>
2024-06-25 20:42:02 +0530

    modified continue reading

--- src/components/Blogs/ContinueReading.tsx
+++ src/components/Blogs/ContinueReading.tsx
@@ -36,7 +36,7 @@ export default function ContinueReading() {
                     <div className="col-md-6 px-0">
                         <h1 className="display-4 font-italic">{Blog.title}</h1>
                         <p className="lead my-3">{Blog.summary}</p>
-                        <ReactMarkdown>{Blog.content}</ReactMarkdown>
+                        {/* <ReactMarkdown>{Blog.content.substring(0,100)}</ReactMarkdown> */}
                         <p className="lead mb-0"><a href="#" onClick={handleClick} className="text-white font-weight-bold">Continue reading...</a></p>
                     </div>
                 </div>)}
